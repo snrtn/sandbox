a5b448cc41bc4a5e7793e32e53e037a755fe8d41
Jun <hanjun.kim.dev@gmail.com>
2024-09-11 14:06:26 +0200

    commit 11.09.2024

--- portfolio/api/src/index.ts
+++ portfolio/api/src/index.ts
@@ -1,54 +1,54 @@
-import express from 'express';
-import connectDB from './config/db';
-import authRoutes from './routes/authRoutes';
-import blogRoutes from './routes/blogRoutes';
-import dashboardRoutes from './routes/dashboardRoutes';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
-import errorHandler from './middlewares/errorHandler';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import connectDB from "./config/db";
+import authRoutes from "./routes/authRoutes";
+import blogRoutes from "./routes/blogRoutes";
+import dashboardRoutes from "./routes/dashboardRoutes";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import errorHandler from "./middlewares/errorHandler";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
 const corsOptions = {
-	origin: process.env.ORIGIN || 'https://portfront-six.vercel.app',
-	credentials: true,
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization'],
-	preflightContinue: false,
-	optionsSuccessStatus: 204,
+  origin: process.env.ORIGIN || "https://portfolioui-nu.vercel.app/",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-	console.log('CORS settings applied:', corsOptions);
-	next();
+  console.log("CORS settings applied:", corsOptions);
+  next();
 });
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cookieParser());
 
 connectDB();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/blog', blogRoutes);
-app.use('/api/dashboard', dashboardRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/blog", blogRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
