1510f40cf131e0c703fcf40efdc96871fa16063c
Jun <hanjun.kim.dev@gmail.com>
2024-09-12 11:19:23 +0200

    commit 12.09.2024

--- portfolio/api/src/index.ts
+++ portfolio/api/src/index.ts
@@ -14,9 +14,13 @@ dotenv.config();
 
 const app = express();
 
+// CORS 설정
 app.use(
   cors({
-    origin: "https://portfolioui-nu.vercel.app",
+    origin: "https://portfolioui-nu.vercel.app", // 허용할 도메인
+    credentials: true, // 자격 증명(쿠키 등)을 허용
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // 허용할 HTTP 메소드
+    allowedHeaders: ["Content-Type", "Authorization"], // 허용할 헤더
   })
 );
 
